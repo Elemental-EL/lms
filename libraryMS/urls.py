0f5937b8a254e87b87569cd3c836e6fa0cf1e579
Elyar KordKatool <elyar.k@yahoo.com>
2024-09-11 21:19:47 +0330

    Created the Viewsets and urls for the models, there are more changes to be made to the Viewsets in the future.

--- libraryMS/urls.py
+++ libraryMS/urls.py
@@ -4,9 +4,15 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from libraryMS.views import NotificationViewSet
+from libraryMS.views import AuthorViewSet, BorrowerViewSet, BookViewSet, BorrowingTransactionViewSet, ReservationViewSet, ReviewViewSet, NotificationViewSet
 
 router = DefaultRouter()
+router.register(r'authors', AuthorViewSet)
+router.register(r'borrowers', BorrowerViewSet)
+router.register(r'books', BookViewSet)
+router.register(r'borrowings', BorrowingTransactionViewSet)
+router.register(r'reservations', ReservationViewSet)
+router.register(r'reviews', ReviewViewSet)
 router.register(r'notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
--- libraryMS/views.py
+++ libraryMS/views.py
@@ -4,10 +4,87 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from libraryMS.models import Notification
-from libraryMS.serializers import NotificationSerializer
+from libraryMS.models import Author, Borrower, Book, BorrowingTransaction, Reservation, Review, Notification
+from libraryMS.serializers import (
+    NotificationSerializer,
+    AuthorSerializer,
+    BorrowerSerializer,
+    BookSerializer,
+    BorrowingTransactionSerializer,
+    ReservationSerializer,
+    ReviewSerializer
+)
+from libraryMS.permissions import (
+    IsAdmin,
+    IsAuthorOrReadOnly,
+    IsOwnerOrReadOnly,
+    CanReserveBook,
+    CanBorrowBook,
+    CanReviewBook,
+    IsBorrower
+)
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+
+# Author ViewSet
+class AuthorViewSet(viewsets.ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        actionf = getattr(self, 'action', None)
+        if actionf in ['update', 'partial_update', 'destroy']:
+            return [IsOwnerOrReadOnly()]
+        return super().get_permissions()
+
+
+# Borrower ViewSet
+class BorrowerViewSet(viewsets.ModelViewSet):
+    queryset = Borrower.objects.all()
+    serializer_class = BorrowerSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        actionf = getattr(self, 'action', None)
+        if actionf in ['update', 'partial_update', 'destroy']:
+            return [IsOwnerOrReadOnly()]
+        return super().get_permissions()
+
+
+# Book ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdmin | IsAuthorOrReadOnly]
+
+
+# Borrowing Transaction ViewSet
+class BorrowingTransactionViewSet(viewsets.ModelViewSet):
+    queryset = BorrowingTransaction.objects.all()
+    serializer_class = BorrowingTransactionSerializer
+    permission_classes = [CanBorrowBook]
+
+
+# Reservation ViewSet
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+    permission_classes = [CanReserveBook | IsBorrower]
+
+
+# Review ViewSet
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [CanReviewBook | IsBorrower]
+
+    def get_permissions(self):
+        actionf = getattr(self, 'action', None)
+        if actionf in ['update', 'partial_update', 'destroy']:
+            return [IsBorrower()]
+        return super().get_permissions()
 
 
 class NotificationViewSet(viewsets.ModelViewSet):
